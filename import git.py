f8cb3e78a3578a89e9968cb374b7f3da90a10708
AMR\snehasir <sneha.siripurapu@intel.com>
2022-02-08 17:13:01 -0600

    changes

--- import git.py
+++ import git.py
@@ -4,6 +4,7 @@ test = "This is a test file"
 print(test)
 print("This is a test addition")
 print("This is test2")
+print("This is test3")
 
 remoteurl = "git@github.com:snehasiripurapu/Test.git"
 #localfolder = "/codefromgit"
