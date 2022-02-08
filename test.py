5574ca888f99901ec16c7a167e9337f522e546aa
AMR\snehasir <sneha.siripurapu@intel.com>
2022-02-08 14:13:42 -0600

    initial release

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
+import git
+
 test = "This is a test file"
 print(test)
+
+remoteurl = "git@github.com:snehasiripurapu/Test.git"
+#localfolder = "/codefromgit"
+
+myrepo = git.cmd.Git(remoteurl)
+myrepo.pull()
